bd0e5420faeaf8db63693dabb8ea90bb4d0ab65d
demosofa <62540039+demosofa@users.noreply.github.com>
2021-02-26 22:22:37 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,10 +1,10 @@
 <?php
-define('HOST','ec2-54-87-34-201.compute-1.amazonaws.com');
-define('DATABASE' , 'd7drsbv5bd202n');
-define('USERNAME' , 'epcqrbjgylnpjk');
-define('PASSWORD' , '0ea10e0dd21c9abc5ef0106d7729dd684732a35faa5c850198558e4e2866bbd6');
+$host = 'ec2-54-87-34-201.compute-1.amazonaws.com';
+$dbname = 'd7drsbv5bd202n';
+$user_db = 'epcqrbjgylnpjk';
+$pw_db = '0ea10e0dd21c9abc5ef0106d7729dd684732a35faa5c850198558e4e2866bbd6';
 
-$conn = pg_connect(HOST, USERNAME, PASSWORD, DATABASE);
+$conn = pg_connect("host=$host port=5432 dbname=$dbname user=$user_db password=$pw_db");
 if (!$conn) {
 die("Connection failed: ".pg_connect_error());
 }
