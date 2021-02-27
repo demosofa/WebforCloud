c6a3755138363dfaebfed4fd6e2e6c2d5f5a5fd0
demosofa <62540039+demosofa@users.noreply.github.com>
2021-02-27 21:35:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-ini_set('display_errors',1);
-error_reporting(-1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'config.php';
 if(isset($_POST['login'])) {
 	$username = $_POST['username'];
