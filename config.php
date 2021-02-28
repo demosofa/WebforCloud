e70c33eda4e8a41bb2dc361f066bf9fef5fe1a90
demosofa <62540039+demosofa@users.noreply.github.com>
2021-02-28 22:35:03 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $dbname = 'd7drsbv5bd202n';
 $user_db = 'epcqrbjgylnpjk';
 $pw_db = '0ea10e0dd21c9abc5ef0106d7729dd684732a35faa5c850198558e4e2866bbd6';
 
-$conn = pg_connect("host=$host port=5432 dbname=$dbname user=$user_db password=$pw_db");
+$conn = pg_connect("host="$host" port=5432 dbname="$dbname" user="$user_db" password="$pw_db"");
 if (!$conn) {
 die("Connection failed: ".pg_connect_error());
 }
