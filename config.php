a83f7393d5d1889f10664318902c4f3bcbd52708
demosofa <62540039+demosofa@users.noreply.github.com>
2021-02-28 22:37:06 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php
-$host = 'ec2-54-87-34-201.compute-1.amazonaws.com';
-$dbname = 'd7drsbv5bd202n';
-$user_db = 'epcqrbjgylnpjk';
-$pw_db = '0ea10e0dd21c9abc5ef0106d7729dd684732a35faa5c850198558e4e2866bbd6';
 
-$conn = pg_connect("host="$host" port=5432 dbname="$dbname" user="$user_db" password="$pw_db"");
+$conn = pg_connect("host=ec2-54-87-34-201.compute-1.amazonaws.com port=5432 dbname=d7drsbv5bd202n user=epcqrbjgylnpjk password=0ea10e0dd21c9abc5ef0106d7729dd684732a35faa5c850198558e4e2866bbd6");
 if (!$conn) {
 die("Connection failed: ".pg_connect_error());
 }
