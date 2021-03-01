7a99f80269f9d1e5344599f870648de1cb428fb5
demosofa <62540039+demosofa@users.noreply.github.com>
2021-03-02 00:19:50 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
-
-$conn = pg_connect("host=ec2-54-87-34-201.compute-1.amazonaws.com port=5432 dbname=d7drsbv5bd202n user=epcqrbjgylnpjk password=0ea10e0dd21c9abc5ef0106d7729dd684732a35faa5c850198558e4e2866bbd6");
-if (!$conn) {
-die("Connection failed: ".pg_connect_error());
+define('USER', 'epcqrbjgylnpjk');
+define('PASSWORD', '0ea10e0dd21c9abc5ef0106d7729dd684732a35faa5c850198558e4e2866bbd6');
+define('HOST', 'ec2-54-87-34-201.compute-1.amazonaws.com');
+define('DATABASE', 'd7drsbv5bd202n');
+try {
+    $connection = new PDO("pgsql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
 ?>
