3cc983c33630b6de76617bfd937f7b9f013fde29
demosofa <62540039+demosofa@users.noreply.github.com>
2021-03-02 00:41:58 +0700

    Update view.php

--- view.php
+++ view.php
@@ -4,9 +4,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require 'config.php';
-
-$id = $_GET['view'];
-
+?>
+<?php $id = $_GET['view']; ?>
+<?php
 if(isset($_GET['Edit'])){
 	global $id;
 	$product = $_GET['Edit'];
