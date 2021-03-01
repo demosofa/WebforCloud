9e1847b45b1de74e3da4ac658798980b3b4a5573
demosofa <62540039+demosofa@users.noreply.github.com>
2021-03-02 01:49:32 +0700

    Update manage.php

--- manage.php
+++ manage.php
@@ -1,15 +1,6 @@
 <?php
 include("config.php");
-if(isset($_GET['edit'])){
-		$id = $_GET['edit'];
-		$update = true;
-		$query = $connection->prepare("SELECT * FROM manageuser WHERE id=:id");
-		$query->bindParam("id", $id, PDO::PARAM_INT);
-		$query->execute();
-		$result = $query->fetch(PDO::FETCH_ASSOC);
-		$user = $result['manager'];
-		$email = $result['email'];
-		$andress = $result['andress'];
+
 }?>
 
 <!DOCTYPE html>
