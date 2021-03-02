34ac0eb6dbc54753cb9908e8d051011ce89f70b8
demosofa <62540039+demosofa@users.noreply.github.com>
2021-03-02 19:06:00 +0700

    Update server.php

--- server.php
+++ server.php
@@ -9,18 +9,11 @@ $update = false;
 if(isset($_GET['view'])){
 	$id = $_GET['view'];
 	$_SESSION['ID'] = $id;
-	header("location: server.php");
-	exit;
-}
-
-if(isset($_SESSION['ID'])){
-	$id = $_SESSION['ID'];
-	$query = $connection->prepare("SELECT * FROM storedata WHERE id=:id");
-	$query->bindParam("id", $id, PDO::PARAM_INT);
-	$query->execute();
 	header("location: view.php");
 	exit;
 }
+
+
 
 if(isset($_POST['Updatedata'])){
 	$id = $_POST['Updatedata'];
