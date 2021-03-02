ecc3ccf361a923c246a9dfef6fa3fdedbc1f9b93
demosofa <62540039+demosofa@users.noreply.github.com>
2021-03-02 13:25:38 +0700

    Update manage.php

--- manage.php
+++ manage.php
@@ -27,51 +27,6 @@ $update = false;
 		$email = $result['email'];
 		$andress = $result['andress'];
 	}?>
-	<?php
-	if(isset($_POST['save'])){
-		// khai bao value and ngan ngua van de postgresql injection
-		$id = $_POST['id'];
-		$manager = $_POST['manager'];
-		$password = $_POST['password'];
-		$email = $_POST['email'];
-		$andress = $_POST['andress'];
-		// run query
-		$sql = "INSERT INTO manageuser(id, manager, pwd, email, andress) VALUES(?,?,?,?,?)";
-		$connection->prepare($sql)->execute([$id, $manager, $password, $email, $andress]);
-		// luu message at server side and toi manage.php page
-		$_SESSION['message'] = "Address saved"; 
-		header("location: manage.php");
-		exit;
-	}?>
-	<?php
-	if(isset($_POST['update'])){
-		$id = $_POST['id'];
-		$manager = $_POST['manager'];
-		$password = $_POST['password'];
-		$email = $_POST['email'];
-		$andress = $_POST['andress'];
-		$data = [
-			'id' => $id,
-			'manager' => $manager,
-			'password' => $password,
-			'email' => $email,
-			'andress'=> $andress,
-		];
-		$sql = "UPDATE manageuser SET manager=:manager, pwd=:password, email=:email, andress=:andress WHERE id=:id;
-		$connection->prepare($sql)->execute($data);
-		$_SESSION['message'] = "Address updated!";
-		header("location: manage.php");
-		exit;
-	}?>
-	<?php
-	if(isset($_GET['del']){
-		$id = $_GET['del'];
-		$sql = "DELETE FROM manageuser WHERE id=?";
-		$connection->prepare($sql)->execute([$id]);
-		$_SESSION['message'] = "Andress deleted";
-		header("location: manage.php");
-		exit;
-	}?>
 	<?php if (isset($_SESSION['message'])): ?>
 	<div class="msg">
 		<?php 
